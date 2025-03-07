eb2159b47e574ec51493fe55a8070d95b10adc05
Edumachadoh <dudu.henrique.em@gmail.com>
2025-03-07 15:50:38 -0300

    Read me

--- ProjetoDenuncias/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ ProjetoDenuncias/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13acbe3b0e4f26428e6a3998b47f1e46b0bbdda4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+052183c36d363d5d289216035e7622b11b752a33")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
