fa80c50c61d471ec223d911d20371dd1c8a84d90
Edumachadoh <dudu.henrique.em@gmail.com>
2025-03-12 15:42:48 -0300

    Edição funcionando API

--- ProjetoDenuncias/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ ProjetoDenuncias/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daea3ae15160ca68ad5de6cd2aaf5ac1cdb6a2a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd52b9d7baddb49f56c08bbc52716b1dc2b15a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
